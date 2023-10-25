6605ca2beec5df6bf650c6949e89e803d2acfc16
Raghav Anandan <raghav.anandan@raghav.anandan>
2023-10-25 10:12:36 -0700

    Remove skip_provider_registration field

--- modules/services/service-principal/main.tf
+++ modules/services/service-principal/main.tf
@@ -1,5 +1,4 @@
 provider "azurerm" {
-  skip_provider_registration = true
   features {}
 }
 
@@ -11,9 +10,9 @@ data "azurerm_subscription" "primary" {
 # Create service principal in customer tenant
 #---------------------------------------------------------------------------------------------
 resource "azuread_service_principal" "sysdig_sp" {
-  # Using Sysdig's application ID in sysdigqatenant tenant.
+  # Using Sysdig's application ID from customer1 tenant.
   # TODO: use application id from sysdig tenant
-  application_id = "0a800625-0e0d-4805-827c-743371e8518c"
+  application_id = "a39a3795-c3d7-4c8b-9c1a-24ea5011be8a"
 }
 
 #---------------------------------------------------------------------------------------------
@@ -23,4 +22,8 @@ resource "azurerm_role_assignment" "sysdig_reader" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Reader"
   principal_id         = azuread_service_principal.sysdig_sp.object_id
+}
+
+output "subscription_id" {
+  value = azuread_service_principal.sysdig_sp.object_id
 }
