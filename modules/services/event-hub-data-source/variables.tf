ea11aa01048249af0df2114ffcf013c91b48496a
matteopasa <matteo.pasina@sysdig.com>
2023-11-15 10:41:49 +0100

    remove sub and tenant from tf

--- modules/services/event-hub-data-source/variables.tf
+++ modules/services/event-hub-data-source/variables.tf
@@ -1,16 +1,6 @@
-variable "tenant_id" {
-  type = string
-  description = "Customer Tenant Id to onboard"
-}
-
-variable "subscription_id" {
-  type = string
-  description = "Subscription ID in which to create a trust relationship"
-}
-
 variable "sysdig_client_id" {
   type = string
-  description = "Object ID of the enterprise application in the Sysdig tenant"
+  description = "Application ID of the enterprise application in the Sysdig tenant"
 }
 
 variable "partition_count" {
