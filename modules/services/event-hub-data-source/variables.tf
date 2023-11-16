f3ecede6550a91fc0588f9d0991d30c375c9ca87
Giovanni Recupero <giovanni.recupero@sysdig.com>
2023-11-16 09:57:13 +0100

    Add subscription_id variable and use it in the module

--- modules/services/event-hub-data-source/variables.tf
+++ modules/services/event-hub-data-source/variables.tf
@@ -44,3 +44,8 @@ variable "namespace_sku" {
   description = "SKU (Plan) for the namespace that will be created"
   default     = "Standard"
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Identifier of the subscription to be onboarded"
+}
