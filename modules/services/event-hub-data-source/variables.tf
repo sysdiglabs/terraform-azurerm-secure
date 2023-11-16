14f38ff5fd7104023cac079e48b71a575af08682
Giovanni Recupero <giovanni.recupero@sysdig.com>
2023-11-16 15:29:25 +0100

    Revamp vars

--- modules/services/event-hub-data-source/variables.tf
+++ modules/services/event-hub-data-source/variables.tf
@@ -1,24 +1,21 @@
 variable "tenant_id" {
-  description = "The ID of the tenant"
   type        = string
+  description = "Identifier of the Azure tenant"
 }
 
-variable "event_hub_subscription_id" {
-  description = "The ID of the subscription where to create the Event Hub"
+variable "subscription_id" {
   type        = string
+  description = "Identifier of the subscription to be onboarded"
 }
-
-/*
-variable "subscription_ids" {
-  description = "The IDs of the subscriptions"
-  type        = list(string)
-}
-*/
 
 variable "sysdig_client_id" {
   type        = string
   description = "Application ID of the enterprise application in the Sysdig tenant"
-  default = "07f34d7e-ff4e-44e0-aaa4-2560a5667166"
+}
+
+variable "location" {
+  type        = string
+  description = "Location where Sysdig-related resources will be created"
 }
 
 variable "partition_count" {
@@ -33,19 +30,9 @@ variable "message_retention_days" {
   default     = 1
 }
 
-variable "location" {
-  type        = string
-  description = "Location where Sysdig-related resources will be created"
-  default     = "West Europe"
-}
-
 variable "namespace_sku" {
   type        = string
   description = "SKU (Plan) for the namespace that will be created"
   default     = "Standard"
 }
 
-variable "subscription_id" {
-  type        = string
-  description = "Identifier of the subscription to be onboarded"
-}
