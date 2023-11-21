e12308ee1a3c4389de2c264409a23d4bf9b8cb86
Francesco Secci <francesco.secci@sysdig.com>
2023-11-21 14:34:55 -0800

    Add Host-scanner onboarding script

---
+++ modules/services/host-scanner/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_subscription" "primary" {
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_lighthouse_definition" "lighthouse_definition" {
+  name               = "Sysdig Agentless Host Scanner"
+  description        = "Lighthouse definition for Sysdig Agentless Host Scanner"
+  managing_tenant_id = var.sysdig_tenant_id
+  scope              = "/subscriptions/${var.subscription_id}"
+  authorization {
+    principal_id           = var.sysdig_service_principal_id
+    principal_display_name = "Sysdig Service Principal Agentless Host Scanner"
+    # Uses VM Scanner Operator role
+    role_definition_id = "d24ecba3-c1f4-40fa-a7bb-4588a071e8fd"
+  }
+}
+
+resource "azurerm_lighthouse_assignment" "lighthouse_assignment" {
+  scope                    = "/subscriptions/${var.subscription_id}"
+  lighthouse_definition_id = azurerm_lighthouse_definition.lighthouse_definition.id
+}
---
+++ modules/services/host-scanner/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.76.0"
+    }
+    sysdig = {
+      source  = "sysdiglabs/sysdig"
+      version = ">= 1.15.0"
+    }
+  }
+}
