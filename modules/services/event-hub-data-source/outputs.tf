039dd3536fd1508041f7bf094c3d01b6dd9ed836
matteopasa <matteo.pasina@sysdig.com>
2023-12-13 18:26:14 +0100

    add service principal outputs

--- modules/services/event-hub-data-source/outputs.tf
+++ modules/services/event-hub-data-source/outputs.tf
@@ -16,3 +16,27 @@ output "subscription_alias" {
   value = data.azurerm_subscription.sysdig_subscription.display_name
   description = "Display name of the subscription"
 }
+
+output "service_principal_display_name" {
+  value       = azuread_service_principal.sysdig_sp.display_name
+  description = "Display name of the Service Principal created"
+}
+
+output "service_principal_client_id" {
+  value       = azuread_service_principal.sysdig_sp.client_id
+  description = "Client ID of the Service Principal created"
+}
+output "service_principal_id" {
+    value       = azuread_service_principal.sysdig_sp.id
+    description = "Service Principal ID on the customer tenant"
+}
+
+output "service_principal_app_display_name" {
+    value       = azuread_service_principal.sysdig_sp.display_name
+    description = "Display name of the Application created"
+}
+
+output "service_principal_app_owner_organization_id" {
+    value       = azuread_service_principal.sysdig_sp.application_tenant_id
+    description = "Organization ID of the Application created"
+}
