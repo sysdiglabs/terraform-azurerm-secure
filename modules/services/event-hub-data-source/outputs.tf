9752eea21af5fca82e801d25939a65ca10f8ae3c
matteopasa <matteo.pasina@sysdig.com>
2023-12-15 11:42:54 +0100

    no creation of single ds in org case

--- modules/services/event-hub-data-source/outputs.tf
+++ modules/services/event-hub-data-source/outputs.tf
@@ -7,6 +7,7 @@ output "event_hub_namespace" {
   value       = azurerm_eventhub_namespace.sysdig_event_hub_namespace.name
   description = "Name of the newly created Event Hub Namespace"
 }
+
 output "consumer_group_name" {
     value       = azurerm_eventhub_consumer_group.sysdig_consumer_group.name
     description = "Name of the newly created Event Hub Consumer Group"
