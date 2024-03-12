cb64127a4823755ce977c2d40d1871c2492c5b85
Francesco Secci <francesco.secci@sysdig.com>
2024-03-12 16:46:45 -0700

    use subscriptions id

--- modules/services/host-scanner/organizational.tf
+++ modules/services/host-scanner/organizational.tf
@@ -1,37 +1,25 @@
 #---------------------------------------------------------------------------------------------
 # Fetch the management groups for customer tenant and onboard subscriptions under them
 #---------------------------------------------------------------------------------------------
+# If no management group is present, then the root management group is used to onboard all the subscriptions
 data "azurerm_management_group" "root_management_group" {
   count  = var.is_organizational && length(var.management_group_ids) == 0 ? 1 : 0
   display_name = "Tenant Root Group"
 }
 
-locals {
-  # when empty, this will be the root management group whose default display name is "Tenant root group"
-  management_groups = var.is_organizational && length(var.management_group_ids) == 0 ? [data.azurerm_management_group.root_management_group[0].id] : toset(
-    [for m in var.management_group_ids : format("%s/%s", "/providers/Microsoft.Management/managementGroups",m)])
+data "azurerm_management_group" "management_groups" {
+  for_each = var.is_organizational && length(var.management_group_ids) > 0 ? var.management_group_ids : []
+  name = each.value
 }
 
-resource "azurerm_lighthouse_definition" "lighthouse_definition_for_tenant" {
-  for_each = var.is_organizational ? local.management_groups : []
-
-  provider           = azurerm.subscription
-  name               = "Sysdig Agentless Host Scanner"
-  description        = "Lighthouse definition for Sysdig Agentless Host Scanner"
-  managing_tenant_id = var.sysdig_tenant_id
-  scope              = each.key
-  authorization {
-    principal_id           = var.sysdig_service_principal_id
-    principal_display_name = "Sysdig Service Principal Agentless Host Scanner"
-    # Uses VM Scanner Operator role
-    role_definition_id = "d24ecba3-c1f4-40fa-a7bb-4588a071e8fd"
-  }
+locals {
+  subscriptions = toset(var.is_organizational && length(var.management_group_ids) == 0 ? data.azurerm_management_group.root_management_group[0].subscription_ids :
+    flatten([for m in data.azurerm_management_group.management_groups : m.subscription_ids]))
 }
 
 resource "azurerm_lighthouse_assignment" "lighthouse_assignment_for_tenant" {
-  for_each = var.is_organizational ? local.management_groups : []
+  for_each = var.is_organizational ? toset(local.subscriptions) : toset([])
 
-  provider                 = azurerm.subscription
-  scope                    = each.key
+  scope                    = "/subscriptions/${each.value}"
   lighthouse_definition_id = azurerm_lighthouse_definition.lighthouse_definition.id
 }
