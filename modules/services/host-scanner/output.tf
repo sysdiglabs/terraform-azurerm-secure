9bfb7f736fd2d6deff496f864647db9b31974470
Francesco Secci <francesco.secci@sysdig.com>
2024-03-19 09:39:42 -0700

    add subscription alias

--- modules/services/host-scanner/output.tf
+++ modules/services/host-scanner/output.tf
@@ -2,3 +2,8 @@ output "lighthouse_definition_display_id" {
   value       = azurerm_lighthouse_definition.lighthouse_definition.id
   description = "Display id of the Light House definition created"
 }
+
+output "subscription_alias" {
+  value = data.azurerm_subscription.primary.display_name
+  description = "Display name of the subscription"
+}
