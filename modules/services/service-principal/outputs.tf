272262773b5fc3167bfdfe2d73b38223d56c6b21
James Larrea <97474779+jameslarrea@users.noreply.github.com>
2024-05-21 13:09:18 -0400

    use existing resource group and fmt (#37)
    * use existing resource group and fmt
    
    * simplified variable

--- modules/services/service-principal/outputs.tf
+++ modules/services/service-principal/outputs.tf
@@ -8,18 +8,18 @@ output "service_principal_client_id" {
   description = "Client ID of the Service Principal created"
 }
 output "service_principal_id" {
-    value       = azuread_service_principal.sysdig_sp.id
-    description = "Service Principal ID on the customer tenant"
+  value       = azuread_service_principal.sysdig_sp.id
+  description = "Service Principal ID on the customer tenant"
 }
 
 output "service_principal_app_display_name" {
-    value       = azuread_service_principal.sysdig_sp.display_name
-    description = "Display name of the Application created"
+  value       = azuread_service_principal.sysdig_sp.display_name
+  description = "Display name of the Application created"
 }
 
 output "service_principal_app_owner_organization_id" {
-    value       = azuread_service_principal.sysdig_sp.application_tenant_id
-    description = "Organization ID of the Application created"
+  value       = azuread_service_principal.sysdig_sp.application_tenant_id
+  description = "Organization ID of the Application created"
 }
 
 output "subscription_tenant_id" {
@@ -28,6 +28,6 @@ output "subscription_tenant_id" {
 }
 
 output "subscription_alias" {
-  value = data.azurerm_subscription.primary.display_name
+  value       = data.azurerm_subscription.primary.display_name
   description = "Display name of the subscription"
 }
