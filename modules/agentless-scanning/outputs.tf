2bfc96a9ef7baeff88b503497f318585848f460e
cgeers <c.geers@gmail.com>
2024-06-27 12:10:55 -0500

    fix: add dependency to component id outputs (#43)
    Because the values referenced are static, they don't implicitly form a hard dependency on the component resource.  Add an explicit dependency

--- modules/agentless-scanning/outputs.tf
+++ modules/agentless-scanning/outputs.tf
@@ -6,4 +6,5 @@ output "lighthouse_definition_display_id" {
 output "service_principal_component_id" {
   value       = "${sysdig_secure_cloud_auth_account_component.azure_service_principal.type}/${sysdig_secure_cloud_auth_account_component.azure_service_principal.instance}"
   description = "Component identifier of Service Principal created in Sysdig Backend for Agentless Scanning"
+  depends_on = [ sysdig_secure_cloud_auth_account_component.azure_service_principal ]
 }
--- modules/integrations/event-hub/outputs.tf
+++ modules/integrations/event-hub/outputs.tf
@@ -1,4 +1,5 @@
 output "event_hub_component_id" {
   value       = "${sysdig_secure_cloud_auth_account_component.azure_event_hub.type}/${sysdig_secure_cloud_auth_account_component.azure_event_hub.instance}"
   description = "Component identifier of Event Hub integration created in Sysdig Backend for Log Ingestion"
+  depends_on = [ sysdig_secure_cloud_auth_account_component.azure_event_hub ]
 }
