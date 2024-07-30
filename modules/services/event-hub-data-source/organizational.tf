463447d90547dedb909c1c53f4f5fcec14072293
matteopasa <matteo.pasina@sysdig.com>
2024-07-30 11:09:07 +0200

    enable entra only org

--- modules/services/event-hub-data-source/organizational.tf
+++ modules/services/event-hub-data-source/organizational.tf
@@ -49,3 +49,132 @@ resource "azurerm_monitor_diagnostic_setting" "sysdig_org_diagnostic_setting" {
     category = "Policy"
   }
 }
+
+resource "azurerm_monitor_aad_diagnostic_setting" "sysdig_entra_diagnostic_setting" {
+  count = (var.enable_entra && var.is_organizational) ? 1 : 0
+
+  name                           = "${var.entra_diagnostic_settings_name}-${local.subscription_hash}"
+  eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.sysdig_rule.id
+  eventhub_name                  = azurerm_eventhub.sysdig_event_hub.name
+
+  enabled_log {
+    category = "AuditLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "SignInLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "NonInteractiveUserSignInLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "ServicePrincipalSignInLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "ManagedIdentitySignInLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "ProvisioningLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "ADFSSignInLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "RiskyUsers"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "UserRiskEvents"
+
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "NetworkAccessTrafficLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "RiskyServicePrincipals"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "ServicePrincipalRiskEvents"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "EnrichedOffice365AuditLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "MicrosoftGraphActivityLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  enabled_log {
+    category = "RemoteNetworkHealthLogs"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
