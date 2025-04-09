68cd82fc106f6d6858663ab9e76c0cef474697aa
matteopasa <matteo.pasina@sysdig.com>
2025-04-09 17:05:33 +0200

    add Monitoring Reader role

--- modules/integrations/event-hub/main.tf
+++ modules/integrations/event-hub/main.tf
@@ -127,6 +127,15 @@ resource "azurerm_role_assignment" "sysdig_subscription_reader" {
 }
 
 #---------------------------------------------------------------------------------------------
+# Assign "Monitoring Reader" role to Sysdig SP at subscription level to check resource health
+#---------------------------------------------------------------------------------------------
+resource "azurerm_role_assignment" "sysdig_subscription_reader" {
+  scope                = data.azurerm_subscription.sysdig_subscription.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azuread_service_principal.sysdig_event_hub_sp.object_id
+}
+
+#---------------------------------------------------------------------------------------------
 # Create diagnostic settings for the subscription
 #---------------------------------------------------------------------------------------------
 resource "azurerm_monitor_diagnostic_setting" "sysdig_diagnostic_setting" {
