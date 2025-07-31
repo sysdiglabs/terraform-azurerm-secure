3125e5a7970a56bc2924a7ecb5d6e11e21c63702
Jose Pablo Camacho <jose.camacho@sysdig.com>
2025-07-31 17:42:25 -0600

    SSPROD-59007 - include/exclude TF provisioning support

--- modules/vm-workload-scanning/aks-discovery/locals.tf
+++ modules/vm-workload-scanning/aks-discovery/locals.tf
@@ -17,7 +17,7 @@ locals {
   # rule 4: default mode (no filters) - use root management group scope
   
   # determine which scopes to include for resource creation
-  scopes_for_resources = local.check_old_management_group_ids_param ? (
+  scopes_for_resources = var.is_organizational ? (local.check_old_management_group_ids_param ? (
     # legacy mode: use management_group_ids if provided
     toset([for m in var.management_group_ids : format("%s/%s", "/providers/Microsoft.Management/managementGroups", m)])
   ) : length(var.include_management_groups) > 0 ? (
@@ -29,7 +29,7 @@ locals {
   ) : (
     # exclude mode or default mode: use root management group scope (tenant level)
     toset([data.azurerm_management_group.root_management_group[0].id])
-  )
+  )) : []
 }
 
 check "validate_org_configuration_params" {
